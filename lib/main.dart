94e48a1330487d34181c7117ff29886bb8ee4558
Orca-175 <stephorcaa@gmail.com>
2025-11-07 01:49:17 +0800

    Finish adding routing logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:it312_final_project/pages/dashboard.dart';
-import 'package:it312_final_project/pages/guardian_details_form.dart';
-import 'package:it312_final_project/pages/loan_request.dart';
-import 'package:it312_final_project/pages/login.dart';
-import 'package:it312_final_project/pages/profile_details.dart';
-import 'package:it312_final_project/pages/register.dart';
-import 'package:it312_final_project/pages/requests_form.dart';
-import 'package:it312_final_project/pages/student_details_form.dart';
 import 'package:it312_final_project/routes/router_config.dart';
 
 void main() {
@@ -20,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: AppRouter.router,
+      routerConfig: router,
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/routes/router_config.dart
+++ lib/routes/router_config.dart
@@ -3,91 +3,113 @@ import 'package:go_router/go_router.dart';
 import 'package:it312_final_project/pages/dashboard.dart';
 import 'package:it312_final_project/pages/guardian_details_form.dart';
 import 'package:it312_final_project/pages/loan_request.dart';
+import 'package:it312_final_project/pages/login.dart';
 import 'package:it312_final_project/pages/profile_details.dart';
+import 'package:it312_final_project/pages/register.dart';
 import 'package:it312_final_project/pages/requests_form.dart';
 import 'package:it312_final_project/pages/student_details_form.dart';
 
-class AppRouter {
-  static GoRouter router = GoRouter(
-    initialLocation: '/',
-    routes: [
-      StatefulShellRoute.indexedStack(
-        branches: [
-          StatefulShellBranch(
-            routes: [
-              GoRoute(
-                path: '/',
-                builder: (context, state) => Dashboard()
-              ),
-            ],
-          ),
-          StatefulShellBranch(
-            routes: [
-              GoRoute(
-                path: '/profile_details',
-                builder: (context, state) => ProfileDetails(),
-                routes: [
-                  GoRoute(
-                    path: 'student_details_form',
-                    builder: (context, state) => StudentDetailsForm(),
-                  ),
-                  GoRoute(
-                    path: 'guardian_details_form',
-                    builder: (context, state) => GuardianDetailsForm(),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          StatefulShellBranch(
-            routes: [
-              GoRoute(
-                path: '/loan_request',
-                builder: (context, state) => LoanRequest(),
-                routes: [
-                  GoRoute(
-                    path: 'requests_form',
-                    builder: (context, state) => RequestsForm(),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
-        builder: (context, state, navigationShell) {
-          return Scaffold(
-            resizeToAvoidBottomInset: false,
-            appBar: AppBar(
-              centerTitle: true,
-              title: Text('ScholarPay'),
+GoRouter router = GoRouter(
+  initialLocation: '/register', // initialLocation: '/login',
+  routes: [
+    GoRoute(
+      path: '/register',
+      builder: (context, state) => RootScaffold(body: Register()),
+    ),
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => RootScaffold(body: Login()),
+    ),
+    StatefulShellRoute.indexedStack(
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/',
+              builder: (context, state) => Dashboard()
             ),
-            body: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: navigationShell,
-            ),
-            bottomNavigationBar: BottomNavigationBar(
-              currentIndex: navigationShell.currentIndex,
-              onTap: (index) {
-                return navigationShell.goBranch(index);
-              },
-              items: [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: 'Dashboard',
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/profile_details',
+              builder: (context, state) => ProfileDetails(),
+              routes: [
+                GoRoute(
+                  path: 'student_details_form',
+                  builder: (context, state) => StudentDetailsForm(),
                 ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.person),
-                  label: 'Profile Details',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.list),
-                  label: 'Loan Request',
+                GoRoute(
+                  path: 'guardian_details_form',
+                  builder: (context, state) => GuardianDetailsForm(),
                 ),
               ],
             ),
-          );
-        }
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/loan_request',
+              builder: (context, state) => LoanRequest(),
+              routes: [
+                GoRoute(
+                  path: 'requests_form',
+                  builder: (context, state) => RequestsForm(),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+      builder: (context, state, navigationShell) {
+        return RootScaffold(
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: navigationShell,
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: navigationShell.currentIndex,
+            onTap: (index) {
+              return navigationShell.goBranch(index);
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Dashboard',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profile Details',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.list),
+                label: 'Loan Request',
+              ),
+            ],
+          ),
+        );
+      }
+    ),
+  ],
+);
+
+class RootScaffold extends StatelessWidget {
+  final Widget body;
+  final Widget? bottomNavigationBar;
+  const RootScaffold({super.key, required this.body, this.bottomNavigationBar});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('ScholarPay'),
       ),
-    ],
+      body: body,
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
 }
